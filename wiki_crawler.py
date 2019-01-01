2f8ee97aaa035bd027f54e3f9d7a9e6d2a64e9cd
Andy D <andyd2k@hotmail.com>
2019-01-01 18:03:03 -0500

    some changes

--- wiki_crawler.py
+++ wiki_crawler.py
@@ -7,7 +7,7 @@ import time
 
 class WikiCrawler:
     def __init__(self, wiki):
-        self.MAX_P_CHECKS = 1
+        self.MAX_P_CHECKS = 5
         self.MAX_CRAWLS = 1
         self.TARGET = "Philosophy"
         self.DOMAIN = "https://en.wikipedia.org"
@@ -19,22 +19,37 @@ class WikiCrawler:
     def is_valid(element):
         return getattr(element, 'name', None) == 'a' and 'id' not in element.attrs
 
+    def build_url(self, wiki_topic, add_wiki_text):
+        if add_wiki_text:
+            url = self.DOMAIN + '/wiki/' + wiki_topic
+        else:
+            url = self.DOMAIN + wiki_topic
+        return url
+
     def parse_p_tag(self, p):
         next_wiki = None
         contents = p.contents
         stack = []
         for element in contents:
+            # Keeps track of balanced parenthesis to
+            # ensure no links that are within them
+            # are used. Since closing parenthesis
+            # may be within the same string, pop
+            # must be checked immediately
             if isinstance(element, NavigableString):
                 if '(' in element:
                     stack.append('(')
                 if ')' in element:
                     stack.pop()
+            # Checks to see if the stack is empty
+            # meaning now outside of the parenthesis
+            # and can check if a link
             if isinstance(element, Tag) and self.is_valid(element) and not stack:
                 next_wiki = element.attrs['href']
                 return next_wiki
         return next_wiki
 
-    def continue_parsing(self, div):
+    def parse_html(self, div):
         p_tags = div.find_all('p', not {'class': 'mw-empty-elt'},
                               recursive=False, limit=self.MAX_P_CHECKS)
         next_wiki = None
@@ -44,15 +59,7 @@ class WikiCrawler:
                 return next_wiki
         return next_wiki
 
-    def build_url(self, wiki_topic, add_wiki_text):
-        if add_wiki_text:
-            url = self.DOMAIN + '/wiki/' + wiki_topic
-        else:
-            url = self.DOMAIN + wiki_topic
-        return url
-
     def crawler(self):
-        done = False
         cycle_check = set()
 
         print("Start of path: " + self.start_wiki)
@@ -66,13 +73,15 @@ class WikiCrawler:
             title = soup.find('h1', {"id": "firstHeading"})
 
             if title.getText() == self.TARGET:
-                return True
+                print("test")
 
             div = soup.find('div', {'class': 'mw-parser-output'})
 
-            time.sleep(2)
-            wiki = self.continue_parsing(div)
+            wiki = self.parse_html(div)
 
+            # Might lead to a dead end (no links to follow) or
+            # a cycle (first eventually links back to a wiki
+            # page already visited
             if not wiki or wiki in cycle_check:
                 print("INVALID")
                 self.invalid_path += 1
@@ -81,6 +90,8 @@ class WikiCrawler:
                 cycle_check.add(wiki)
                 print(wiki)
                 url = self.build_url(wiki, False)
+
+            time.sleep(2)
 
     def crawl(self):
         i = 0
@@ -93,6 +104,6 @@ class WikiCrawler:
 
 
 if __name__ == '__main__':
-    wiki = "Math"
+    wiki = "Philosophy"
     crawler = WikiCrawler(wiki)
     crawler.crawl()
